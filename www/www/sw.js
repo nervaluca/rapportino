5f6ed6b89ba5e8be14a4c48f633955127fdd0aff
nervaluca <61270102+nervaluca@users.noreply.github.com>
2026-07-23 21:01:04 +0200

    Add files via upload

---
+++ www/www/sw.js
@@ -0,0 +1,36 @@
+// Service worker: rende l'app utilizzabile anche offline (cantieri senza campo)
+const CACHE = 'rapportino-v1';
+const FILES = [
+  './',
+  './index.html',
+  './exceljs.min.js',
+  './manifest.webmanifest',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/apple-touch-icon.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(e.request).then(r => r || caches.match('./index.html')))
+  );
+});
